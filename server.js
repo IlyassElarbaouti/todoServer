9fa860fc6717b1252499925caf11803c50b14d3c
ilyass elarbaouti <mrfixer59@gmail.com>
2022-01-19 17:11:59 +0200

    server

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = 9000;
+const app = express();
+
+let todos = [
+  { label: "test2", id: 0, checked: true },
+  { label: "test3", id: 1, checked: false },
+];
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json());
+
+//get all todos
+app.get("/", (req, res) => res.status(200).json(todos));
+
+//get todo by index
+app.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todoRes = todos.filter(todo => todo.id == id)
+  if (todoRes.length !== 0) {
+    res.status(200).json(todoRes)
+  }
+  else {
+    res.status(404,'The task is not found').send()
+  }
+})
+
+// add new todo
+app.post("/", (req, res) => {
+  var newTodo = req.body;
+  newTodo.id = Math.max(...todos.map(todos=>todos.id))+1
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+// edit existant todo
+app.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todoRes = todos.filter((todo) => todo.id == id);
+  const targetedIndex = todos.findIndex(todo => todo.id === id);
+  if (todoRes.length !== 0) {
+    todos[targetedIndex] = req.body;
+    res.status(204).send();
+  } else {
+    res.status(404, "The task is not found").send();
+  }
+});
+
+//delete todo
+app.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (todos.filter((todo) => todo.id == id).length !== 0) {
+    todos = todos.filter((todo) => todo.id !== id);
+    res.status(200).send();
+  } else {
+    res.status(404, "The task is not found").send();
+  }
+});
+
+app.listen(port);
