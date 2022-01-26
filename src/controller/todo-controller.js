04b9d0b72fdf23f24a8d61b622baa6460184503e
ilyass elarbaouti <mrfixer59@gmail.com>
2022-01-26 11:03:59 +0200

    fixed

--- src/controller/todo-controller.js
+++ src/controller/todo-controller.js
@@ -1,9 +1,9 @@
 const todoRepository = require("../repository/todo-repository");
-
+const todoService = require("../service/todo-service");
 class TodoController {
   constructor() {
     this.todoRepository = todoRepository;
-
+    this.todoService = todoService;
     this.getAllTodos = this.getAllTodos.bind(this);
     this.getTodoById = this.getTodoById.bind(this);
     this.addNewTodo = this.addNewTodo.bind(this);
@@ -15,88 +15,43 @@ class TodoController {
 
   // get all todos
   getAllTodos(req, res) {
-    try {
-      res
-        .status(200)
-        .json(this.todoRepository.getAllTodos());
-    } catch (e) {
-      res.status(404, e.message).send();
-    }
+   this.todoService.getAllTodos(res)
   }
 
   // get todo by id
   getTodoById(req, res) {
     const id = parseInt(req.params.id);
     const todoRes = this.todoRepository.getTodoById(id);
-    if (todoRes) {
-      try {
-        res.status(200).json(todoRes);
-      } catch (e) {
-        res.status(404, e.message).send();
-      }
-    } else {
-      res.status(404, "The task is not found").send();
-    }
+    this.todoService.getTodoById(todoRes,res)
   }
 
   // add new todo
   addNewTodo(req, res) {
     const newTodo = req.body;
-    try {
-      this.todoRepository.createTodo(newTodo);
-      res.status(201).json(newTodo);
-    } catch (e) {
-      res.status(404, e.message).send();
-    }
+    this.todoService.addNewTodo(newTodo, res);
   }
 
   // edit existant todo
   editTodo(req, res) {
     const id = parseInt(req.params.id);
     const todoToEdit = req.body;
-    try {
-      const newTodo = this.todoRepository.editTodo(todoToEdit, id);
-      res.status(204).send(newTodo);
-    } catch (e) {
-      res.status(404, e.message).send();
-    }
+    this.todoService.editTodo(todoToEdit, id, res);
   }
 
   // delete todo
   deleteTodo(req, res) {
     const id = parseInt(req.params.id);
-    const targetedIndex = this.todoRepository.getTodoIndex(id);
-    if (this.todoRepository.todos.some((todo) => todo.id == id)) {
-      try {    
-        this.todoRepository.todos.splice(targetedIndex, 1);
-        res.status(200).send();
-      }
-      catch (e) {   
-        res.status(404, e.message).send();
-      }
-    } else {
-      res.status(404, e.message).send();
-    }
+    this.todoService.deleteTodo(id,res)
   }
 
   //delete all done
   deleteAllDone(req, res) {
-    try {
-      this.todoRepository.deleteAllDone();
-      res.status(200).send();
-    } catch (e) {
-      res.status(404, e.message).send();
-    }
+    this.todoService.deleteAllTodos(res)
   }
 
   //toggle all checked
   toggleAllChecked(req, res) {
-    try {
-      this.todoRepository.toggleAllChecked();
-      res.status(200).send(this.todoRepository.todos);
-    } catch (e) {
-      res.status(404, e.message).send();
-    }
+    this.todoService.toggleAllChecked(res)
   }
 }
 module.exports = new TodoController();
---
+++ src/service/todo-service.js
@@ -0,0 +1,78 @@
+const { todoRepository } = require("../controller/todo-controller");
+
+class TodoService {
+  constructor() {
+    this.todoRepository = todoRepository;
+  }
+
+  getAllTodos(res) {
+    try {
+      res.status(200).json(this.todoRepository.getAllTodos());
+    } catch (e) {
+      res.status(404, e.message).send();
+    }
+  }
+
+  getTodoById(todoRes, res) {
+    if (todoRes) {
+      try {
+        res.status(200).json(todoRes);
+      } catch (e) {
+        res.status(404, e.message).send();
+      }
+    } else {
+      res.status(404, "The task is not found").send();
+    }
+  }
+
+  addNewTodo(newTodo, res) {
+    try {
+      this.todoRepository.createTodo(newTodo);
+      res.status(201).json(newTodo);
+    } catch (e) {
+      res.status(404, e.message).send();
+    }
+  }
+
+  editTodo(todoToEdit, id, res) {
+    try {
+      const newTodo = this.todoRepository.editTodo(todoToEdit, id);
+      res.status(204).send(newTodo);
+    } catch (e) {
+      res.status(404, e.message).send();
+    }
+  }
+
+  deleteTodo(id, res) {
+    if (this.todoRepository.todos.some((todo) => todo.id == id)) {
+      try {
+        const targetedIndex = this.todoRepository.getTodoIndex(id);
+        this.todoRepository.todos.splice(targetedIndex, 1);
+        res.status(200).send();
+      } catch (e) {
+        res.status(404, e.message).send();
+      }
+    } else {
+      res.status(404, e.message).send();
+    }
+  }
+
+  deleteAllDone(res) {
+    try {
+      this.todoRepository.deleteAllDone();
+      res.status(200).send();
+    } catch (e) {
+      res.status(404, e.message).send();
+    }
+  }
+  toggleAllChecked(res) {
+    try {
+      this.todoRepository.toggleAllChecked();
+      res.status(200).send(this.todoRepository.todos);
+    } catch (e) {
+      res.status(404, e.message).send();
+    }
+  }
+}
+
+module.exports = new TodoService();
