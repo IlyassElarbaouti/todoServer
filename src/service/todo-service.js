36a5e349d73baa5d3df8cc5ea570f4cdcc479e8c
ilyass elarbaouti <mrfixer59@gmail.com>
2022-01-26 11:15:25 +0200

    binded

--- src/service/todo-service.js
+++ src/service/todo-service.js
@@ -3,6 +3,13 @@ const { todoRepository } = require("../controller/todo-controller");
 class TodoService {
   constructor() {
     this.todoRepository = todoRepository;
+    this.getAllTodos = this.getAllTodos.bind(this);
+    this.getTodoById = this.getTodoById.bind(this);
+    this.addNewTodo = this.addNewTodo.bind(this);
+    this.editTodo = this.editTodo.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+    this.deleteAllDone = this.deleteAllDone.bind(this);
+    this.toggleAllChecked = this.toggleAllChecked.bind(this);
   }
 
   getAllTodos(res) {
